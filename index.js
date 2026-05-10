27074b71ffe0118682c0dd8d4384bf027e35abf1
pbelopez <ed101801@gmail.com>
2026-05-10 07:41:04 -0600

    chang java to 130

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
   window.addEventListener('scroll', function() {
     const nav = document.querySelector('nav');
     // Change 120 to whatever pixel value feels right (test on mobile/desktop)
-    if (window.scrollY > 120) {
+    if (window.scrollY > 130) {
       nav.classList.add('scrolled');
     } else {
       nav.classList.remove('scrolled');
     }
   });
+
+
+  
